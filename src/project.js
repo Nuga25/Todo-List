f8b77c16324952fb98f129a1f1ab3d1be5f9ae37
Nuga25 <osinugaifeoluwa1@gmail.com>
2024-12-24 17:59:00 +0100

    CRUD functions for projects and todos

--- src/project.js
+++ src/project.js
@@ -1,23 +1,46 @@
-let arrOfProjects = [];
-
+//function to create new projects
 function createProject(name) {
-  const project = {
-    name: name,
-    tasks: [],
-    addTasks(task) {
-      this.tasks.push(task);
-    },
-    removeTasks(taskName) {
-      this.tasks = this.tasks.filter((task) => task !== taskName);
-    },
+  const tasks = [];
+
+  const addTask = (task) => {
+    tasks.push(task);
   };
 
+  const removeTask = (taskName) => {
+    const index = tasks.findIndex((task) => task.title === taskName);
+    tasks.splice(index, 1);
+  };
+
+  const getTasks = () => {
+    return [...tasks];
+  };
+
+  return {
+    name,
+    addTask,
+    removeTask,
+    getTasks,
+  };
+}
+
+let arrOfProjects = [];
+
+//function to add project to array
+function addProject(name) {
+  const project = createProject(name);
   arrOfProjects.push(project);
   return project;
 }
 
+//function to edit project name
+function editProjectName(oldName, newName) {
+  const project = arrOfProjects.find((proj) => proj.name === oldName);
+  return (project.name = newName);
+}
+
+//function to delete project
 function deleteProject(name) {
   return (arrOfProjects = arrOfProjects.filter((proj) => proj.name !== name));
 }
 
-export { arrOfProjects, createProject, deleteProject };
+export { arrOfProjects, addProject, editProjectName, deleteProject };
--- src/todo.js
+++ src/todo.js
@@ -1,12 +1,22 @@
-import { arrOfProjects, createProject, deleteProject } from "./project.js";
+import {
+  arrOfProjects,
+  addProject,
+  editProjectName,
+  deleteProject,
+} from "./project.js";
 
 //function to create todo
-function createTodoItem(title, description, duedate, priority, projectName) {
-  const task = { title, description, duedate, priority };
+function createTodoItem(title, description, duedate, priority) {
+  return { title, description, duedate, priority };
+}
+
+//function to add todo to a project
+function addTodoToProject(title, description, duedate, priority, projectName) {
+  const task = createTodoItem(title, description, duedate, priority);
 
   const project = arrOfProjects.find((proj) => proj.name === projectName);
   if (project) {
-    project.addTasks(task);
+    project.addTask(task);
   } else {
     console.log("project name does not exist");
   }
@@ -14,35 +24,93 @@ function createTodoItem(title, description, duedate, priority, projectName) {
   return task;
 }
 
-function deleteTodo(project, i) {
-  //...
+//function to edit todo
+function editTodo(
+  newTitle,
+  newDescription,
+  newDuedate,
+  newPriority,
+  todoTitle,
+  projectName
+) {
+  const project = arrOfProjects.find((proj) => proj.name === projectName);
+  const newTodo = project.getTasks().find((task) => task.title === todoTitle); // or `todo.title`
+
+  newTodo.title = newTitle;
+  newTodo.description = newDescription;
+  newTodo.duedate = newDuedate;
+  newTodo.priority = newPriority;
+
+  return newTodo;
 }
 
-createProject("PROJECT 1");
-createTodoItem(
+//function to delete todo from project
+function deleteTodoFromProject(title, projectName) {
+  const project = arrOfProjects.find((proj) => proj.name === projectName);
+  if (project) {
+    project.removeTask(title);
+  } else {
+    console.log("task does not exist");
+  }
+}
+
+addProject("PROJECT 1");
+addTodoToProject(
   "make dinner",
   "rice flour for four",
   "today",
   "medium",
   "PROJECT 1"
 );
-createTodoItem("make bed", "before you lay down", "today", "high", "PROJECT 1");
+addTodoToProject(
+  "make bed",
+  "before you lay down",
+  "today",
+  "high",
+  "PROJECT 1"
+);
 
-createProject("PROJECT 2");
-createTodoItem(
+addProject("PROJECT 2");
+addTodoToProject(
   "make dinner",
   "rice flour for four",
   "today",
   "medium",
   "PROJECT 2"
 );
-createTodoItem("make bed", "before you lay down", "today", "high", "PROJECT 2");
-createTodoItem("make bed", "before you lay down", "today", "high", "PROJECT 2");
+addTodoToProject(
+  "make bed",
+  "before you lay down",
+  "today",
+  "high",
+  "PROJECT 2"
+);
+addTodoToProject(
+  "make bed",
+  "before you lay down",
+  "today",
+  "high",
+  "PROJECT 2"
+);
+deleteProject("PROJECT 1");
+
+deleteTodoFromProject("make dinner", "PROJECT 2");
+
+editTodo(
+  "new title",
+  "new description",
+  "23-12-24",
+  "high",
+  "make bed",
+  "PROJECT 2"
+);
+
+editProjectName("PROJECT 2", "Arghhhhhh!!!!");
 
 //Log all projects and their tasks
 arrOfProjects.forEach((project) => {
   console.log(`Project: ${project.name}`);
-  project.tasks.forEach((task) => {
+  project.getTasks().forEach((task) => {
     console.log(`- Task: ${task.title}, Due: ${task.duedate}`);
   });
 });
