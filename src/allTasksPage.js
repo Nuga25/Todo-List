b2baca692afe009239764c721a6f7007bba86a2b
Nuga25 <osinugaifeoluwa1@gmail.com>
2025-01-08 23:48:38 +0100

    minor UI correction

--- src/allTasksPage.js
+++ src/allTasksPage.js
@@ -56,6 +56,7 @@ export function allTasksPage() {
 
     const illustrationEmptyImg = document.createElement("img");
     illustrationEmptyImg.src = illustrationImg;
+    illustrationEmptyImg.classList.add("emptyPageImg");
     illustrationEmptyImg.width = "450";
     const paragraphText = document.createElement("p");
     paragraphText.textContent =
